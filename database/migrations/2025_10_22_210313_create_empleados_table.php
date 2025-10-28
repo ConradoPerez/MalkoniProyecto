f6f70a7f6d24261271fc6c6bbf7c91c505a549b9
corip <corip@GIGABYTECONRADO>
2025-10-28 15:45:18 -0300

    first commit

---
+++ database/migrations/2025_10_22_210313_create_empleados_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Migración para la tabla Empleados
+Schema::create('empleados', function (Blueprint $table) {
+    $table->id('id_empleado'); // id_empleado integer [primary key]
+    $table->string('nombre', 255);
+    $table->string('foto')->nullable();
+    $table->string('email', 255)->unique();
+    $table->integer('telefono')->nullable();
+    $table->integer('dni')->unique();
+    
+    // Clave Foránea a Roles (id_rol)
+    $table->foreignId('id_rol')->constrained('roles', 'id_rol'); 
+    
+    $table->timestamps();
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
